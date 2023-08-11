5c62f0874529fce41c4c66f5ddec7ea09a4648f0
Amer <tahrijouteyamer@hotmail.com>
2023-08-10 19:05:11 -0700

    .

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+#task 102
+from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
-
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
         return (c)
     else:
-        return(sub(a, b))
+        return sub(a, b)
+...........................................................
+vi 103-fast_alphabet.py
+
+#!/usr/bin/python3
+import string
+print(string.ascii_uppercase)
