04f6e7805032c3534e0f4af49b4ea2b16b0167cc
Amer <tahrijouteyamer@hotmail.com>
2023-09-11 16:00:48 -0700

    .

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines Rectangle module."""
-Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__('99-rectangle').Rectangle
 
 
 class Square(Rectangle):
--- 0x0A-python-inheritance/99-rectangle.py
+++ 0x0A-python-inheritance/99-rectangle.py
@@ -17,4 +17,4 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return "[Square] {}/{}".format(self.__width, self.__height)
