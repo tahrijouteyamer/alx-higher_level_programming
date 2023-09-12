1b3d4c12a84a4949fe80a4292d11c09b941b16e2
Amer <tahrijouteyamer@hotmail.com>
2023-09-12 16:07:10 -0700

    .

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,16 +6,22 @@ class Student:
     """Represents a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes student props.
+        """Initializes a new student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
 
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-
